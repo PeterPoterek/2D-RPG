34260a2580e12181975527983aef4b7c000486b0
Piotr Poterek <kapusniakpl@gmail.com>
2022-12-26 13:41:00 +0100

    Fixed blurry sprites when player is moving

--- AI Only Project/Assets/Enemy.cs
+++ AI Only Project/Assets/Enemy.cs
@@ -10,21 +10,48 @@ public class Enemy : MonoBehaviour
 
  BoxCollider2D enemyCollider;
  public BoxCollider2D playerCollider;
-
+ Rigidbody2D enemyRigidbody;
  Animator enemyAnimator;
+
+ public float speed = 0.5f;
+ Vector3 moveDirection;
+ public Transform target;
  void Start()
  {
   currentHealth = maxHealth;
+  target = GameObject.FindWithTag("Player").transform;
+
+
   enemyAnimator = GetComponent<Animator>();
   enemyCollider = GetComponent<BoxCollider2D>();
+  enemyRigidbody = GetComponent<Rigidbody2D>();
   playerCollider = FindObjectOfType<PlayerMovement>().gameObject.GetComponent<BoxCollider2D>();
  }
 
  void Update()
  {
 
+
+ }
+ void FixedUpdate()
+ {
+
+  ChaseTarget();
  }
 
+ void ChaseTarget()
+ {
+  // Calculate the position that the enemy should move to
+  // by moving towards the player's position at the specified speed
+  Vector2 targetPosition = Vector2.MoveTowards(
+      enemyRigidbody.position,
+      target.transform.position,
+      speed * Time.deltaTime
+  );
+
+  // Move the enemy to the calculated position
+  enemyRigidbody.MovePosition(targetPosition);
+ }
  public void TakeDamage(int damage)
  {
   if (isDead)
--- AI Only Project/Assets/PlayerMovement.cs
+++ AI Only Project/Assets/PlayerMovement.cs
@@ -146,12 +146,12 @@ public class PlayerMovement : MonoBehaviour
  void MovePlayer()
  {
 
-
   float horizontalInput = Input.GetAxis("Horizontal");
   transform.position += new Vector3(horizontalInput, 0, 0) * ControlPlayerSpeed() * Time.deltaTime;
 
   FlipSpirte();
   CheckIfMoving();
+
 
  }
 
