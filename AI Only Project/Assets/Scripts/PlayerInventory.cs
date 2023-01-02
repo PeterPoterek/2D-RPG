c7a263782c3a5a644ac1d9dd57c1d82961f585c0
Piotr Poterek <kapusniakpl@gmail.com>
2023-01-02 18:34:11 +0100

    Player Inventory Part 3 UI

--- AI Only Project/Assets/Scripts/PlayerInventory.cs
+++ AI Only Project/Assets/Scripts/PlayerInventory.cs
@@ -6,6 +6,9 @@ public class PlayerInventory : MonoBehaviour
 {
  public List<WeaponItem> weaponInventory;
  public WeaponItem currentWeapon;
+ public GameObject playerInventoryWindow;
+ public bool playerInventoryActive;
+ public GameObject inventorySlot;
  // Start is called before the first frame update
  void Start()
  {
@@ -15,12 +18,29 @@ public class PlayerInventory : MonoBehaviour
  // Update is called once per frame
  void Update()
  {
+  TogglePlayerInventory(playerInventoryActive);
+ }
+ void TogglePlayerInventory(bool isActive)
+ {
+  if (Input.GetKeyDown(KeyCode.Tab) && playerInventoryActive == false)
+  {
+   playerInventoryWindow.SetActive(true);
+   playerInventoryActive = true;
+  }
+  else if (Input.GetKeyDown(KeyCode.Tab) && playerInventoryActive == true)
+  {
+   playerInventoryWindow.SetActive(false);
+   playerInventoryActive = false;
+  }
+
 
  }
-
  public void AddItem(WeaponItem weaponItem)
  {
   weaponInventory.Add(weaponItem);
+  GameObject newInventorySlot = Instantiate(inventorySlot, playerInventoryWindow.transform);
+  newInventorySlot.SetActive(true);
+
  }
  public void RemoveItem(WeaponItem weaponItem)
  {
